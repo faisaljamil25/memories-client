13b623ccb5a07df119ef9529f5672c08bd576b3a
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-18 01:11:52 +0530

    feat: redux setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts);
+  }, [dispatch]);
+
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
